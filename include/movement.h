e6492a25ba56171dad8b134150e656a570838055
lowrene <lowrene02@gmail.com>
2023-10-13 00:28:54 +0800

    uncomment movement files

--- include/movement.h
+++ include/movement.h
@@ -1,37 +1,37 @@
-// /*
-//  * Author: Joshua Chua
-//  * Description: TODO: Add description
-//  */
-// #ifndef MOVEMENT_H
-// #define MOVEMENT_H
+/*
+ * Author: Joshua Chua
+ * Description: TODO: Add description
+ */
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
 
-// #endif // MOVEMENT_H
+#endif // MOVEMENT_H
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include "pico/stdlib.h"
-// #include "hardware/pwm.h"
-// #include "hardware/gpio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "hardware/gpio.h"
 
-// // Direction Pins
-// #define L298N_INPUT_1 2
-// #define L298N_INPUT_2 3
-// #define L298N_INPUT_3 4
-// #define L298N_INPUT_4 5
+// Direction Pins
+#define L298N_INPUT_1 2
+#define L298N_INPUT_2 3
+#define L298N_INPUT_3 4
+#define L298N_INPUT_4 5
 
-// // Speed GPIO Pins
-// #define L298N_PIN_ENA 0 //Channel: 0A
-// #define L298N_PIN_ENB 1 //Channel: 0B
-// #define SPEED_SLICE_NUM 0
+// Speed GPIO Pins
+#define L298N_PIN_ENA 0 //Channel: 0A
+#define L298N_PIN_ENB 1 //Channel: 0B
+#define SPEED_SLICE_NUM 0
 
-// //Frequency & Speed Variables
-// // 125Mhz/ CLOCK_DIVIDER * WRAP_VALUE = Frequency wanted, currently, 20Hz is enough.
-// // For Documentation: Lower the frequency, the lesser power, the lesser the reactivity.
-// #define CLOCK_DIVIDER 200
-// #define WRAP_VALUE 31250
+//Frequency & Speed Variables
+// 125Mhz/ CLOCK_DIVIDER * WRAP_VALUE = Frequency wanted, currently, 20Hz is enough.
+// For Documentation: Lower the frequency, the lesser power, the lesser the reactivity.
+#define CLOCK_DIVIDER 200
+#define WRAP_VALUE 31250
 
-// // int l298n_speed_pwm_setup();
-// // int set_speed(uint8_t duty_cycle_left, uint8_t duty_cycle_right);
-// // void set_forward();
-// // void set_backward();
-// // void set_left();
+// int l298n_speed_pwm_setup();
+// int set_speed(uint8_t duty_cycle_left, uint8_t duty_cycle_right);
+// void set_forward();
+// void set_backward();
+// void set_left();
--- src/movement_control.c
+++ src/movement_control.c
@@ -1,133 +1,133 @@
-// /*
-//  * Author: Joshua Chua
-//  * Description: TODO: Add description
-//  */
+/*
+ * Author: Joshua Chua
+ * Description: TODO: Add description
+ */
 
-// #include "../include/movement.h"
+#include "../include/movement.h"
 
-// /**
-//  * l298n_speed_pwm_setup()
-//  * --------
-//  * Purpose: Set up the PWM pins and the GPIO pins to control the direction using the L298N
-//  * Arguments: None
-//  * Return: 1 as completed
-//  */
-// int l298n_speed_pwm_setup()
-// {
-//     // Tell GPIO 0 and 1 they are allocated to the PWM
-//     gpio_set_function(L298N_PIN_ENA, GPIO_FUNC_PWM);
-//     gpio_set_function(L298N_PIN_ENB, GPIO_FUNC_PWM);
+/**
+ * l298n_speed_pwm_setup()
+ * --------
+ * Purpose: Set up the PWM pins and the GPIO pins to control the direction using the L298N
+ * Arguments: None
+ * Return: 1 as completed
+ */
+int l298n_speed_pwm_setup()
+{
+    // Tell GPIO 0 and 1 they are allocated to the PWM
+    gpio_set_function(L298N_PIN_ENA, GPIO_FUNC_PWM);
+    gpio_set_function(L298N_PIN_ENB, GPIO_FUNC_PWM);
 
-//     // Serial Input Output Configuration
-//     gpio_set_function(L298N_INPUT_1, GPIO_FUNC_SIO);
-//     gpio_set_function(L298N_INPUT_2, GPIO_FUNC_SIO);
-//     gpio_set_function(L298N_INPUT_3, GPIO_FUNC_SIO);
-//     gpio_set_function(L298N_INPUT_4, GPIO_FUNC_SIO);
+    // Serial Input Output Configuration
+    gpio_set_function(L298N_INPUT_1, GPIO_FUNC_SIO);
+    gpio_set_function(L298N_INPUT_2, GPIO_FUNC_SIO);
+    gpio_set_function(L298N_INPUT_3, GPIO_FUNC_SIO);
+    gpio_set_function(L298N_INPUT_4, GPIO_FUNC_SIO);
 
-//     //Set all the relevant GPIO pins to output for the L298N
-//     gpio_set_dir(L298N_INPUT_1, GPIO_OUT);
-//     gpio_set_dir(L298N_INPUT_2, GPIO_OUT);
-//     gpio_set_dir(L298N_INPUT_3, GPIO_OUT);
-//     gpio_set_dir(L298N_INPUT_4, GPIO_OUT);
+    //Set all the relevant GPIO pins to output for the L298N
+    gpio_set_dir(L298N_INPUT_1, GPIO_OUT);
+    gpio_set_dir(L298N_INPUT_2, GPIO_OUT);
+    gpio_set_dir(L298N_INPUT_3, GPIO_OUT);
+    gpio_set_dir(L298N_INPUT_4, GPIO_OUT);
 
-//     return 1;
-// }
+    return 1;
+}
 
-// /**
-//  * set_speed()
-//  * --------
-//  * Purpose: Set the speed using the PWM for the car to move. 
-//  *          This function makes the car start
-//  * Arguments: duty_cycle of the PWM. Range from 0 to 1, 1 being 100%
-//  * Return: 1 as completed
-//  */
-// int set_speed(float duty_cycle)
-// {
-//     // // Divide the clock by 125Mhz / 200 = 625 000
-//     pwm_set_clkdiv(SPEED_SLICE_NUM, CLOCK_DIVIDER); 
+/**
+ * set_speed()
+ * --------
+ * Purpose: Set the speed using the PWM for the car to move. 
+ *          This function makes the car start
+ * Arguments: duty_cycle of the PWM. Range from 0 to 1, 1 being 100%
+ * Return: 1 as completed
+ */
+int set_speed(float duty_cycle)
+{
+    // // Divide the clock by 125Mhz / 200 = 625 000
+    pwm_set_clkdiv(SPEED_SLICE_NUM, CLOCK_DIVIDER); 
 
-//     // // Divide the Clock into 12500 Virtual Clock cycles
-//     // // Frequency = 625 000 / 31250 = 20 Hz as stated in the specifications
-//     pwm_set_wrap(SPEED_SLICE_NUM, WRAP_VALUE);
+    // // Divide the Clock into 12500 Virtual Clock cycles
+    // // Frequency = 625 000 / 31250 = 20 Hz as stated in the specifications
+    pwm_set_wrap(SPEED_SLICE_NUM, WRAP_VALUE);
 
-//     // // Set the Duty cycle of the PWN signal to be 50% by dividing by 2 on channel A (PWM GPIO 2 is on Channel A)
-//     pwm_set_chan_level(SPEED_SLICE_NUM, PWM_CHAN_A, WRAP_VALUE * duty_cycle);
-//     pwm_set_chan_level(SPEED_SLICE_NUM, PWM_CHAN_B, WRAP_VALUE * duty_cycle);
+    // // Set the Duty cycle of the PWN signal to be 50% by dividing by 2 on channel A (PWM GPIO 2 is on Channel A)
+    pwm_set_chan_level(SPEED_SLICE_NUM, PWM_CHAN_A, WRAP_VALUE * duty_cycle);
+    pwm_set_chan_level(SPEED_SLICE_NUM, PWM_CHAN_B, WRAP_VALUE * duty_cycle);
 
-//     pwm_set_enabled(0, true);
+    pwm_set_enabled(0, true);
 
-//     return 1;
-// }
+    return 1;
+}
 
-// // Set the car to run forward
-// void set_forward()
-// {
-//     gpio_put(L298N_INPUT_1, 1);
-//     gpio_put(L298N_INPUT_2, 0);
-//     gpio_put(L298N_INPUT_3, 1);
-//     gpio_put(L298N_INPUT_4, 0);
-// }
+// Set the car to run forward
+void set_forward()
+{
+    gpio_put(L298N_INPUT_1, 1);
+    gpio_put(L298N_INPUT_2, 0);
+    gpio_put(L298N_INPUT_3, 1);
+    gpio_put(L298N_INPUT_4, 0);
+}
 
-// // Set the car to run backward
-// void set_backward()
-// {
-//     gpio_put(L298N_INPUT_1, 0);
-//     gpio_put(L298N_INPUT_2, 1);
-//     gpio_put(L298N_INPUT_3, 0);
-//     gpio_put(L298N_INPUT_4, 1);
-// }
+// Set the car to run backward
+void set_backward()
+{
+    gpio_put(L298N_INPUT_1, 0);
+    gpio_put(L298N_INPUT_2, 1);
+    gpio_put(L298N_INPUT_3, 0);
+    gpio_put(L298N_INPUT_4, 1);
+}
 
-// // Set the car to turn left
-// void set_left()
-// {
-//     gpio_put(L298N_INPUT_1, 1);
-//     gpio_put(L298N_INPUT_2, 0);
-//     gpio_put(L298N_INPUT_3, 1);
-//     gpio_put(L298N_INPUT_4, 0);
-// }
+// Set the car to turn left
+void set_left()
+{
+    gpio_put(L298N_INPUT_1, 1);
+    gpio_put(L298N_INPUT_2, 0);
+    gpio_put(L298N_INPUT_3, 1);
+    gpio_put(L298N_INPUT_4, 0);
+}
 
-// // Set the car to turn right
-// void set_right()
-// {
-//     gpio_put(L298N_INPUT_1, 0);
-//     gpio_put(L298N_INPUT_2, 1);
-//     gpio_put(L298N_INPUT_3, 0);
-//     gpio_put(L298N_INPUT_4, 1);
-// }
+// Set the car to turn right
+void set_right()
+{
+    gpio_put(L298N_INPUT_1, 0);
+    gpio_put(L298N_INPUT_2, 1);
+    gpio_put(L298N_INPUT_3, 0);
+    gpio_put(L298N_INPUT_4, 1);
+}
 
-// // Stops the car
-// void set_stop()
-// {
-//     gpio_put(L298N_INPUT_1, 0);
-//     gpio_put(L298N_INPUT_2, 0);
-//     gpio_put(L298N_INPUT_3, 0);
-//     gpio_put(L298N_INPUT_4, 0);
-// }
+// Stops the car
+void set_stop()
+{
+    gpio_put(L298N_INPUT_1, 0);
+    gpio_put(L298N_INPUT_2, 0);
+    gpio_put(L298N_INPUT_3, 0);
+    gpio_put(L298N_INPUT_4, 0);
+}
 
-// // Sameple Main to run the function (To be Deleted After Submission)
-// // int main() {
-// //     stdio_init_all();
+// Sameple Main to run the function (To be Deleted After Submission)
+// int main() {
+//     stdio_init_all();
     
-// //     l298n_speed_pwm_setup();
+//     l298n_speed_pwm_setup();
 
-// //     set_speed(0.5);
+//     set_speed(0.5);
 
-// //     while (1)
-// //     {
-// //         printf("Looping...");
-// //         set_forward();
+//     while (1)
+//     {
+//         printf("Looping...");
+//         set_forward();
 
-// //         if(gpio_get(L298N_INPUT_1))
-// //         {
-// //             printf("Forward");
-// //         }
-// //         sleep_ms(1000);
-// //         set_backward();
-// //         if(gpio_get(L298N_INPUT_2))
-// //         {
-// //             printf("backward");
-// //         }
-// //         sleep_ms(1000);
+//         if(gpio_get(L298N_INPUT_1))
+//         {
+//             printf("Forward");
+//         }
+//         sleep_ms(1000);
+//         set_backward();
+//         if(gpio_get(L298N_INPUT_2))
+//         {
+//             printf("backward");
+//         }
+//         sleep_ms(1000);
         
-// //     }
-// // }
+//     }
+// }
