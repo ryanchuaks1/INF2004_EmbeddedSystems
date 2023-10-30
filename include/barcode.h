8cb4bc2c8997966dc8bc50433b5f19340692306f
Ryan Chua <ryanchuaks2000@gmail.com>
2023-10-30 13:39:27 +0800

    Barcode Algo Complete

--- include/barcode.h
+++ include/barcode.h
@@ -23,13 +23,16 @@
 
 #define DEBOUNCE_DELAY_MS 100
 
-#define BARCODE_THRESHOLD 1000
+#define BARCODE_THRESHOLD 1500
+#define BARCODE_CHAR_LIMIT 2
 
 void barcode_init();
 int get_ir_reading();
 void check_if_wall();
 void init_read_barcode();
 void read_barcode();
+void reset_barcode_params();
+void barcode_to_char();
 
 enum BarType
 {
@@ -55,15 +58,7 @@ static int black_bar_times[] = {0, 0, 0, 0, 0}; // Array for black bar times
 static int white_bar_times[] = {0, 0, 0, 0, 0};    // Array for white bar times
 static int bar_index = 0;                          // Index for the current bar
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)       \
-    ((byte)&0x80 ? '1' : '0'),     \
-        ((byte)&0x40 ? '1' : '0'), \
-        ((byte)&0x20 ? '1' : '0'), \
-        ((byte)&0x10 ? '1' : '0'), \
-        ((byte)&0x08 ? '1' : '0'), \
-        ((byte)&0x04 ? '1' : '0'), \
-        ((byte)&0x02 ? '1' : '0'), \
-        ((byte)&0x01 ? '1' : '0')
+static char code_39_characters[] = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ-. *"; // Array of characters for code 39
+
 
 #endif // BARCODE_H
--- src/barcode.c
+++ src/barcode.c
@@ -16,6 +16,7 @@ void reset_barcode_params()
     coded_barcode = 0;
     decoded_barcode = 0;
     bar_index = 0;
+    gpio_set_irq_enabled_with_callback(WALL_SENSOR_PIN, GPIO_IRQ_EDGE_RISE, true, &check_if_wall); // enable rising edge interrupt
 }
 
 void barcode_init()
@@ -25,11 +26,6 @@ void barcode_init()
     adc_init();
     adc_gpio_init(BARCODE_SENSOR_PIN);
     adc_select_input(BARCODE_ADC_CHANNEL);
-
-    while (true)
-    {
-        printf("Reading: %d\n", get_ir_reading());
-    }
 
     reset_barcode_params();
 
@@ -82,6 +78,9 @@ void decode_barcode(int black_bar_times[], int white_bar_times[])
     dec_white_bar_times[2] = (black_bar_times[3] - white_bar_times[2]) / 10000;
     dec_white_bar_times[3] = (black_bar_times[4] - white_bar_times[3]) / 10000;
 
+    // printf("Black bar times: %d %d %d %d %d\n", dec_black_bar_times[0], dec_black_bar_times[1], dec_black_bar_times[2], dec_black_bar_times[3], dec_black_bar_times[4]);
+    // printf("White bar times: %d %d %d %d\n\n", dec_white_bar_times[0], dec_white_bar_times[1], dec_white_bar_times[2], dec_white_bar_times[3]);
+
     int max1 = 0;
     int max2 = 0;
 
@@ -101,7 +100,7 @@ void decode_barcode(int black_bar_times[], int white_bar_times[])
     // Set the two highest to 1, the rest to 0 for black bars
     for (int i = 0; i < 5; i++)
     {
-        if (dec_black_bar_times[i] == max1 || black_bar_times[i] == max2)
+        if (dec_black_bar_times[i] == max1 || dec_black_bar_times[i] == max2)
         {
             dec_black_bar_times[i] = 1;
         }
@@ -134,17 +133,60 @@ void decode_barcode(int black_bar_times[], int white_bar_times[])
         }
     }
     barcodeFlags.limitter++;
+    printf("\nthis is the %d time\n", barcodeFlags.limitter);
     printf("Black bar times: %d %d %d %d %d\n", dec_black_bar_times[0], dec_black_bar_times[1], dec_black_bar_times[2], dec_black_bar_times[3], dec_black_bar_times[4]);
     printf("White bar times: %d %d %d %d\n\n", dec_white_bar_times[0], dec_white_bar_times[1], dec_white_bar_times[2], dec_white_bar_times[3]);
+    barcode_to_char(dec_black_bar_times, dec_white_bar_times);
+}
+
+void barcode_to_char(int black_bar_times[], int white_bar_times[])
+{
+    printf("Decoding barcode");
+
+    int result = 0;
+
+    if (black_bar_times[0] && black_bar_times[4])
+        result += 1;
+    else if (black_bar_times[1] && black_bar_times[4])
+        result += 2;
+    else if (black_bar_times[0] && black_bar_times[1])
+        result += 3;
+    else if (black_bar_times[2] && black_bar_times[4])
+        result += 4;
+    else if (black_bar_times[0] && black_bar_times[2])
+        result += 5;
+    else if (black_bar_times[1] && black_bar_times[2])
+        result += 6;
+    else if (black_bar_times[3] && black_bar_times[4])
+        result += 7;
+    else if (black_bar_times[0] && black_bar_times[3])
+        result += 8;
+    else if (black_bar_times[1] && black_bar_times[3])
+        result += 9;
+    else if (black_bar_times[2] && black_bar_times[3])
+        result += 10;
+
+    if (white_bar_times[1])
+        result += 0;
+    else if (white_bar_times[2])
+        result += 9;
+    else if (white_bar_times[3])
+        result += 19;
+    else if (white_bar_times[0])
+        result += 29;
+
+    printf("Result: %d\n", result);
+    char decoded_char = code_39_characters[result];
+    printf("Decoded character: %c\n", decoded_char);
 }
 
 void read_barcode()
 {
-    vTaskDelay(1000);
+    vTaskDelay(pdMS_TO_TICKS(1000));
     while (barcodeFlags.isBarcode)
     {
         uint16_t reading = get_ir_reading();
-        printf("Reading: %d\n", reading);
+        // printf("Reading: %d\n", reading);
 
         if (reading > BARCODE_THRESHOLD && !barcodeFlags.isPrevBlackBar)
         {
@@ -166,11 +208,11 @@ void read_barcode()
             white_bar_times[4] = 0;
             bar_index = 0;
         }
-        if (barcodeFlags.limitter > 3)
+        if (barcodeFlags.limitter > BARCODE_CHAR_LIMIT)
         {
             reset_barcode_params();
             vTaskDelete(NULL);
         }
-        vTaskDelay(10);
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
