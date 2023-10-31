3ccd27bc7a0ef7030d7b4b400c55976250ef81dc
Faraway2307 <farahbmt@gmail.com>
2023-10-31 13:58:55 +0800

    updated ultrasonic with custom interrupt

--- include/ultrasonic.h
+++ include/ultrasonic.h
@@ -8,19 +8,13 @@
 #include "common.h"
 #include "hardware/timer.h"
 #include "hardware/gpio.h"
+#include "pico/stdlib.h"
 
 #define BAUD_RATE 115200
 #define TRIG_PIN 0
 #define ECHO_PIN 1
 #define SPEED_OF_SOUND 34300
 
-#define TRIG_PIN 0
-#define ECHO_PIN 1
-#define SPEED_OF_SOUND 34300
-
 void ultrasonic_init();
 void test_ultrasonic();
-void timer_callback(struct repeating_timer *t);
-void gpio_callback(uint gpio, uint32_t events);
-
 #endif
--- src/ultrasonic.c
+++ src/ultrasonic.c
@@ -1,6 +1,10 @@
-#include "../include/ultrasonic.h"
+#include "ultrasonic.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "hardware/gpio.h"
 
 volatile bool measurement_completed = false;
+volatile bool measure_distance_flag = false;
 
 void timer_callback(struct repeating_timer *t)
 {
@@ -9,10 +13,8 @@ void timer_callback(struct repeating_timer *t)
 
 void gpio_callback(uint gpio, uint32_t events)
 {
-    // Handle GPIO interrupt events
-    // In this case, it can be used to detect the rising and falling edges on the ECHO_PIN
-    // Start and stop a timer to measure the pulse width for distance calculation
     static uint32_t start_time = 0;
+
     if (events & GPIO_IRQ_EDGE_RISE)
     {
         start_time = time_us_32();
@@ -21,8 +23,7 @@ void gpio_callback(uint gpio, uint32_t events)
     {
         uint32_t end_time = time_us_32();
         uint32_t pulse_width = end_time - start_time;
-        // Calculate the distance based on the pulse width
-        float distance_cm = pulse_width * SPEED_OF_SOUND / (2 * 1000000); // in centimeters
+        float distance_cm = pulse_width * SPEED_OF_SOUND / (2 * 1000000);
         if (distance_cm >= 4 && distance_cm <= 398)
         {
             printf("Distance: %.2f cm\n", distance_cm);
@@ -41,36 +42,52 @@ void ultrasonic_init()
     gpio_set_dir(TRIG_PIN, GPIO_OUT);
     gpio_init(ECHO_PIN);
     gpio_set_dir(ECHO_PIN, GPIO_IN);
-
-    // Configure interrupt for the echo pin
     gpio_set_irq_enabled_with_callback(ECHO_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 }
 
-void test_ultrasonic()
+void custom_timer_callback()
 {
-    // Generate the trigger pulse
-    gpio_put(TRIG_PIN, 1);
-    sleep_us(10); // Generate a precise delay of 10 microseconds
-    gpio_put(TRIG_PIN, 0);
+    measure_distance_flag = true;
+}
 
-    // Wait for the measurement to complete using the custom interrupt
-    measurement_completed = false;
+bool custom_timer_callback_wrapper(struct repeating_timer *rt)
+{
+    custom_timer_callback();
+    return true;
+}
+
+void custom_delay_us(uint32_t us)
+{
     while (!measurement_completed)
     {
         tight_loop_contents();
     }
+    busy_wait_us(us);
+    measurement_completed = false;
 }
+
+void test_ultrasonic()
+{
+    gpio_put(TRIG_PIN, 1);
+    custom_delay_us(10);
+    gpio_put(TRIG_PIN, 0);
+
+    while (!measure_distance_flag)
+    {
+        tight_loop_contents();
+    }
+    measure_distance_flag = false;
+}
+
 
 int main()
 {
     stdio_init_all();
     ultrasonic_init();
 
-    // Configure and start the timer interrupt
     struct repeating_timer timer;
-    add_repeating_timer_us(1000000, timer_callback, NULL, &timer); // 1-second interval
+    add_repeating_timer_us(500000, custom_timer_callback_wrapper, NULL, &timer);
 
-    // Start a loop to measure the distance
     while (true)
     {
         test_ultrasonic();
